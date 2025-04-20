680674bc63ec8bb89480b9cfa56f5164e3db33e5
Peter Kure <peterkure256@gmail.com>
2025-04-20 23:06:33 +0300

    Feat: Main scaffolding for bottom Navbar

---
+++ lib/main_scaffold.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'screens/categories_screen.dart';
+import 'screens/profile_screen.dart';
+import 'screens/settings_screen.dart';
+
+class MainScaffold extends StatefulWidget {
+  final int initialIndex;
+  const MainScaffold({Key? key, this.initialIndex = 0}) : super(key: key);
+
+  @override
+  State<MainScaffold> createState() => _MainScaffoldState();
+}
+
+class _MainScaffoldState extends State<MainScaffold> {
+  late int _selectedIndex;
+
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    CategoriesScreen(),
+    ProfileScreen(),
+    SettingsScreen(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedIndex = widget.initialIndex;
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: _screens[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.deepPurple,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
